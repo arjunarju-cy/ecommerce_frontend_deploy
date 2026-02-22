9bde1c28bf8c9af61856eaab90e99108aa8b7464
Arjun <arjuni5acer12@gmail.com>
2026-02-22 17:05:11 +0530

    ef

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { store } from './app/store.js';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import axios from 'axios'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    <ToastContainer/>
-    </Provider>
+    <ToastContainer />
+  </Provider>
   // </StrictMode>,
 )
 
-// Use deployed backend in production; keep relative paths in development to allow Vite proxy
+// Axios configuration
+axios.defaults.withCredentials = true;
+
 if (import.meta.env.PROD && import.meta.env.VITE_BACKEND_URL) {
   axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
 }
+
+// Add token to every request header
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
